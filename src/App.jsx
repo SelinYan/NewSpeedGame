8c4b9fbc55ddc725a05eb4d66d5d3ff5d65ce7bc
Margit Tennosaar <margit.tennosaar@bc.fi>
2023-11-28 14:40:03 +0200

    add conditional screens for launch, game on and game over

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from "react"
 import NewGame from "./components/NewGame"
 import Circle from "./UI_components/Circle"
 import Game from "./components/Game"
+import GameOver from "./components/GameOver"
 import { levels } from './levels'
 
 function App() {
   const [player, setPlayer] = useState()
   const [circles, setCircles] = useState([])
-  const [score, setScore] = useState(10)
+  const [score, setScore] = useState(0)
+  const [gameLaunch, setGameLaunch] = useState(true)
+  const [gameOn, setGameOn] = useState(false)
+  const [gameOver, setGameOver] = useState(false)
 
   const gameSetHandler = (level, name) => {
     const levelIndex = levels.findIndex(el => el.name === level);
@@ -21,13 +25,21 @@ function App() {
         name: name
       }
     )
+    setGameLaunch(!gameLaunch)
+    setGameOn(!gameOn)
+  }
+
+  const stopHandler = () => {
+    setGameOn(!gameOn)
+    setGameOver(!gameOver)
   }
 
   return (
     <>
       <h1>Catch the snow!</h1>
-      <NewGame onclick={gameSetHandler} />
-      <Game score={score} circles={circles} />
+      {gameLaunch && <NewGame onclick={gameSetHandler} />}
+      {gameOn && <Game score={score} circles={circles} stopHandler={stopHandler} />}
+      {gameOver && <GameOver />}
     </>
   )
 }
